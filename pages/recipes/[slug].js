f0d395be57daebb864269fa7fe10b92a19cfb348
Pipo300 <caracoloro@gmail.com>
2021-04-14 19:19:55 -0400

    redirec-bug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -38,7 +38,7 @@ export async function getStaticProps({ params }) {
 	if (!items.length) {
 		return {
 			redirect: {
-				destination: '/false',
+				destination: '/',
 				permanent: false,
 			},
 		};
